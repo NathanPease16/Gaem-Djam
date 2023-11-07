8f245d3260e1584b6cb441ad4740b713ace2cb4a
MagiGuy <jdjamrosz@stu.naperville203.org>
2023-11-07 13:31:06 -0600

    Changed Version to 2022.3.9f1
    Also some changes to formatting and very simple jumping

--- Gaem Djam/Assets/Scripts/PlayerMovement.cs
+++ Gaem Djam/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 60f;
-    [SerializeField] private float uprightPower = 50f;
+    [SerializeField] private float jumpForce = 2400f;
 
     private Transform viewer;
     private Rigidbody rb;
@@ -18,24 +18,21 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb.AddForce(GetInput() * moveSpeed, ForceMode.Acceleration); // Apply forces based off wasd
-        rb.AddForce(Vector3.down * rb.mass, ForceMode.Force); // Apply gravity
-        Upright();
+        
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E)) { rb.AddTorque(transform.right * 10, ForceMode.Impulse); Debug.Log("HAH"); }
+        Jump();
     }
 
     Vector3 GetInput() {
         return (Vector3.ProjectOnPlane(viewer.forward, Vector3.down).normalized*Input.GetAxisRaw("Vertical") + viewer.right*Input.GetAxisRaw("Horizontal")).normalized;
     }
 
-    void Upright() {
-        Vector3 goal = Vector3.up;
-        Vector3 current = transform.up;
-        Vector3 axis = Vector3.Cross(goal, current);
-
-        rb.AddTorque(axis * uprightPower, ForceMode.Force);
+    void Jump() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
